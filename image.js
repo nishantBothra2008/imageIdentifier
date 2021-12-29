6d5533b53b434c8f3272e5c27238b09256bb5945
nishantBothra2008 <96818845+nishantBothra2008@users.noreply.github.com>
2021-12-29 14:52:40 +0530

    Add files via upload

---
+++ image.js
@@ -0,0 +1,77 @@
+function preload() {}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier("MobileNet", modelLoaded);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+
+previousResult = "";
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        if ((results[0].confidence > 0.5) && (previousResult != results[0].label)) {
+            console.log(results);
+            previousResult = results[0].label;
+            synth = window.speechSynthesis;
+            speak_data = "Object Detected is " + results[0].label;
+            utterThis = new SpeechSynthesisUtterance(speak_data);
+            synth.speak(utterThis);
+            document.getElementById("object_list").innerHTML = results[0].label;
+            document.getElementById("accuracy_amount").innerHTML = results[0].confidence.toFixed(3);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
